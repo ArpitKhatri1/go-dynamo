51fd56ed86c7ae26e3a5974b42ab8d8201e72727
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2026-05-28 16:43:56 +0530

    finished server setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"dynamo/pkg/server"
+	"flag"
+)
+
+var (
+	seedNodePorts = []int{8000}
 )
 
 func main() {
-	port := flags
-	fmt.Println("Hello world")
+	// parse the CMD flags
+	port := flag.Int("port", 8080, "Port of the server")
+	virtualNode := flag.Int("vn", 1, "Number of Virtual nodes of server")
+	seedNode := flag.Bool("seedNode", false, "seed Node or not")
+	serverId := flag.Int("serverId", 1, "serverId")
+
+	flag.Parse()
+
+	serverConfig := server.NewServerConfig(*serverId, *virtualNode, *port, *seedNode, seedNodePorts)
+
+	server := server.NewServer(serverConfig)
+
+	server.Run()
+
 }
